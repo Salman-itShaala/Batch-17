0b3619936ad34992835514fe0063b9521c7dcf0c
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-24 11:46:19 +0530

    added descision making statements and truthy falsy

---
+++ day - 17/main.js
@@ -0,0 +1,126 @@
+// if(condition){
+//     // code to excute if cond. is true
+// }
+
+
+// if - else
+
+// given age decide whether that person can vote or not
+
+
+
+
+
+// let age = 17;
+
+// if(age >= 18){
+//     console.log("You can vote");
+// }else{
+//     console.log("You can not vote");
+// }
+
+// check if that number is completely divisible by 5
+// if yes then print --> given number is divisible by 5
+// if not then print --> given number is not divisible by 5
+
+
+// let num = 21;
+
+
+// if(num % 5 == 0){
+//     console.log("given number is divisible by 5");
+// }else{
+//     console.log("given number is not divisible by 5");
+// }
+
+
+// print "Awesome" if given number is divisible by 3 and 5
+// otherwise print "Not awesome"
+
+// let givenNum = 55;
+
+// if(givenNum % 15 == 0){
+//     console.log("Awesome");
+// }else{
+//     console.log("Not awesome");
+// }
+
+
+
+let day = "5";
+
+// ===
+switch(day){
+    case 1: 
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3: 
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid value of day");
+}
+
+
+
+// Invalid
+// Friday
+
+// if(day == 1){
+//     console.log("Monday");
+// }else if(day == 2){
+//     console.log("Tuesday");
+// }else if(day == 3){
+//     console.log("Wednesday");
+// }else if(day == 4){
+//     console.log("Thursday");
+// }else if(day == 5){
+//     console.log("Friday")
+// }else if(day == 6){
+//     console.log("Saturday .... kab ayega bhai sunday??");
+// }else if(day == 7){
+//     console.log("Sunday");
+// }else{
+//     console.log("Invalid value of day");
+// }
+
+
+
+
+// in if block {} are optional
+// you can not write more than one statment if you are omitting {}
+// and that single statment can not be a  varible declaration and intilisation --> let somehting = 435;
+
+let marks = 34;
+
+// passed (35)
+
+if(marks >= 90) 
+   console.log("lkndfglkdn");
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day - 17/script.js
@@ -0,0 +1,15 @@
+if("."){
+    console.log("truthy")
+}else{
+    console.log("falsy");
+}
+
+// "", '', `` --> 
+// " "
+
+// "salman" "  salman"
+
+// Error
+// truthy
+// falsy
+
