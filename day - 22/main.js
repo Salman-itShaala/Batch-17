c9d73eb41d64d91d17337fb273acfd9b3a1021e2
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-30 11:20:02 +0530

    added function code

---
+++ day - 22/main.js
@@ -0,0 +1,16 @@
+// write a function which will take marks (number) as input and add 5 to that
+// number and will return grace marks
+
+function addGraceMarks(marks) {
+  if (marks >= 35) {
+    return marks;
+  }
+
+  const graceMarks = marks + 5;
+  return graceMarks;
+}
+
+for (let i = 0; i <= 10; i++) {
+  break;
+  console.log("Hiii");
+}
