1e68cdbddf5368dcd69fcac1b8643fe3bedae01b
salman-itShaala <salman.shaikh@itshaala.com>
2025-07-31 10:55:58 +0530

    added logic building solutions

---
+++ day - 23/main.js
@@ -0,0 +1,22 @@
+// Task: Write a function that takes two numbers and returns their sum.
+
+function calculateSum(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
+}
+
+//  Create a function that checks if a given number is even or odd.
+// Even
+// Odd
+
+function checkEvenOrOdd(num) {
+  if (num % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+let returnNum = largest(3, 9, 7);
+
+console.log(returnNum);
