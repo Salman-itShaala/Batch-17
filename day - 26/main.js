159ba0ee8d0ef1df899eac9b24d71473a8d0ef2e
salman-itShaala <salman.shaikh@itshaala.com>
2025-08-06 12:55:25 +0530

    added dom - 01 code

---
+++ day - 26/main.js
@@ -0,0 +1,12 @@
+function changeContent() {
+  const h1 = document.getElementById("heading");
+
+  // inline styles
+  //   h1.style.color = "blue";
+  //   h1.style.backgroundColor = "yellow";
+  //   h1.style.fontSize = "50px";
+
+  h1.style.cssText = "color: red; background-color: black; font-size: 50px;";
+}
+
+// changeContent();
