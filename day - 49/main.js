27c2a5dfc6184a2824fb044f7e0f6438c139cef3
salman-itShaala <salman.shaikh@itshaala.com>
2025-09-24 16:07:55 +0530

    adding dynamic data to front end

---
+++ day - 49/main.js
@@ -0,0 +1,42 @@
+const recipesContainer = document.getElementById("recipes-container");
+
+/*
+<a href="*">
+    <img src="images/burger.png" alt="" />
+    <p class="recipe-name">Veg crispy burger</p>
+    <p class="learn-more">Learn more...</p>
+</a>
+
+*/
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const res = await fetch("https://dummyjson.com/recipes?limit=4&skip=0&select=name,image");
+
+    const data = await res.json();
+
+    const recipes = data.recipes; // [{id, image, name}, {}, {}, {}]
+
+    for (let i = 0; i < recipes.length; i++) {
+
+        const aTag = document.createElement("a");
+
+        // 1.
+        const imgTag = document.createElement("img");
+
+        imgTag.src = recipes[i].image;
+
+        // 2.
+        const paraTag = document.createElement("p");
+
+        paraTag.innerText = recipes[i].name;
+        paraTag.className = "recipe-name";
+
+        aTag.append(imgTag, paraTag);
+
+
+        recipesContainer.append(aTag);
+
+    }
+
+})
