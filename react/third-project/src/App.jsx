2b1e7893b8788349b96ed58d2a48a6677b091c42
salman-itShaala <salman.shaikh@itshaala.com>
2025-10-06 15:55:23 +0530

    added react project

---
+++ react/third-project/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <h1>I'm app component</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/third-project/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+  let userName = "Salman";
+
+  let person = {
+    userName: "salman",
+    age: 33,
+  };
+
+  return (
+    <div className="child-1">
+      <h3>Hello welcome back {}</h3>
+    </div>
+  );
+}
+
+export default Header;
